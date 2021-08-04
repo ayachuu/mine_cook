315d0fd6ef0d72dbd35c4d342249e6259b7d3fd8
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-08-04 11:11:59 +0900

    アソシエーション、バリデート記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :text, length: { maximum: 1000, message: '作り方、材料は1,000文字以内で入力してください' }
+    with_options numericality: { other_than: 1, message: "選択してください" } do
+      validates :jenre_id
+    end
+  end
+
 end
