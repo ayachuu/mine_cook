ed927262727d10eed11f3528a2bce5ed0a65f25d
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-08-04 16:11:27 +0900

    メニュー登録機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
     @post = Post.all
     @random = Post.order("RAND()").limit(1)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :image, :jenre_id).merge(user_id: current_user.id)
+  end
 end
