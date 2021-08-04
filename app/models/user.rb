da7975be066f70b512003e3ee65d28c8d0efbd8a
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-08-04 12:05:38 +0900

    passwordのバリデート記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '英字と数字の両方を含めて設定してください' }
 
   has_many :posts
 end
