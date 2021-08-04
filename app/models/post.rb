c5a9a5c347e6a169c489c62c6a2cf5bb2c45856e
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-08-04 11:54:51 +0900

    ActiveStorageとのアソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
   belongs_to :jenre
 
   belongs_to :user
-
+  has_one_attached :image
+  
   with_options presence: true do
     validates :title
     validates :text, length: { maximum: 1000, message: '作り方、材料は1,000文字以内で入力してください' }
