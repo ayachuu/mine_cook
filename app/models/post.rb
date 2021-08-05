f1e4ec2954c6f1f29d031905ad80ef3d1b0f9089
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-08-05 12:35:11 +0900

    一覧表示完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :jenre
+  belongs_to :genre
 
   belongs_to :user
   has_one_attached :image
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
     validates :title
     validates :text, length: { maximum: 1000, message: '作り方、材料は1,000文字以内で入力してください' }
     with_options numericality: { other_than: 1, message: "選択してください" } do
-      validates :jenre_id
+      validates :genre_id
     end
   end
 
