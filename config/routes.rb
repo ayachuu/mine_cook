ada00aadaad2ba7ad1c1c0ad3f25d2e30a440d2d
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-08-12 16:56:37 +0900

    削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
-  resources :posts, only:[:index, :new, :create, :show, :edit, :update] do
+  resources :posts do
     collection do 
       get "list_up"
       get "search_genre"
